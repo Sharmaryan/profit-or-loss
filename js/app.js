ea57e4b87fe58608bda70b82d14a9efcaabb8529
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2021-09-21 14:31:09 +0530

    fix : validations
    negative values not be accepted

--- js/app.js
+++ js/app.js
@@ -2,17 +2,17 @@ const initialStockPrice = document.querySelector("#initial-price");
 const stocksQuantity = document.querySelector("#quantity-of-stocks");
 const currentStockPrice = document.querySelector("#current-price");
 
-const changeTheme = document.querySelector('#change-theme');
+const changeTheme = document.querySelector("#change-theme");
 
 const checkBtn = document.querySelector(".check-btn");
 const showMsg = document.querySelector(".show-msg");
 
-const happyGif = document.querySelector('.happy-gif');
-const sadGif = document.querySelector('.sad-gif');
+const happyGif = document.querySelector(".happy-gif");
+const sadGif = document.querySelector(".sad-gif");
 
-happyGif.style.display = 'none';
-sadGif.style.display = 'none';
-showMsg.style.display = 'none';
+happyGif.style.display = "none";
+sadGif.style.display = "none";
+showMsg.style.display = "none";
 
 const calculateProfit = function (initialPrice, currentPrice) {
   const profit = currentPrice - initialPrice;
@@ -30,67 +30,78 @@ const showMessage = function (message) {
   showMsg.innerText = message;
 };
 
-
 function submitHandler() {
   showMsg.style.display = "block";
 
   const priceOfInitialStock = initialStockPrice.value * stocksQuantity.value;
- 
+
   const priceOfCurrentStock = currentStockPrice.value * stocksQuantity.value;
 
-
-  if((priceOfCurrentStock !='') && (priceOfInitialStock != '') && (stocksQuantity.value != '')){
-
-  if (priceOfInitialStock < priceOfCurrentStock) {
-    const calculatedProfit = calculateProfit(
-      priceOfInitialStock,
-      priceOfCurrentStock
-    );
-
-
-    const message = `Hey, the profit is ${calculatedProfit[0].toFixed(2)} and the profit percentage is ${calculatedProfit[1].toFixed(2)}%`;
-
-     changeTheme.style.background = "none";
-    happyGif.style.display = "block";
-
-    sadGif.style.display = "none";
-
-    showMessage(message);
-
-  } else if (priceOfInitialStock > priceOfCurrentStock) {
-
-    const calculatedLoss = calculateLoss(
-      priceOfInitialStock,
-      priceOfCurrentStock
-    );
-
-    
-    const message = `Hey, the loss is ${calculatedLoss[0].toFixed(2)} and the loss percentage is ${calculatedLoss[1].toFixed(2)}%`;
-    if(calculatedLoss[1] > 50){
-      changeTheme.style.background = 'yellow';
-    }
-
-    sadGif.style.display = "block";
-
+  if (
+    priceOfCurrentStock === "" ||
+    priceOfInitialStock === "" ||
+    stocksQuantity.value === ""
+  ) {
+    const message = `Please don't leave any field empty`;
+    showMsg.style.display = "block";
     happyGif.style.display = "none";
-
-    showMessage(message);
-
-  }
-  else{
+    sadGif.style.display = "none";
     changeTheme.style.background = "none";
-    const message = 'No pain no gain and no gain no pain';
     showMessage(message);
-    
+  } else if (
+    priceOfCurrentStock <= 0 ||
+    priceOfInitialStock <= 0 ||
+    stocksQuantity.value <= 0
+  ) {
+    showMsg.style.display = "block";
+    const message = `zero or negative values are not allowed`;
+    happyGif.style.display = "none";
+    sadGif.style.display = "none";
+    changeTheme.style.background = "white";
+    showMessage(message);
+  } else {
+    if (priceOfInitialStock < priceOfCurrentStock) {
+      const calculatedProfit = calculateProfit(
+        priceOfInitialStock,
+        priceOfCurrentStock
+      );
+
+      const message = `Hey, the profit is ${calculatedProfit[0].toFixed(
+        2
+      )} and the profit percentage is ${calculatedProfit[1].toFixed(2)}%`;
+
+      changeTheme.style.background = "none";
+      happyGif.style.display = "block";
+
+      sadGif.style.display = "none";
+
+      showMessage(message);
+    } else if (priceOfInitialStock > priceOfCurrentStock) {
+      const calculatedLoss = calculateLoss(
+        priceOfInitialStock,
+        priceOfCurrentStock
+      );
+
+      const message = `Hey, the loss is ${calculatedLoss[0].toFixed(
+        2
+      )} and the loss percentage is ${calculatedLoss[1].toFixed(2)}%`;
+      if (calculatedLoss[1] > 50) {
+        changeTheme.style.background = "red";
+      }
+
+      sadGif.style.display = "block";
+
+      happyGif.style.display = "none";
+
+      showMessage(message);
+    } else {
+      const message = "No Profit No Loss";
+      showMessage(message);
+      happyGif.style.display = "none";
+      sadGif.style.display = "none";
+      changeTheme.style.background = "yellow";
+    }
   }
-}
-else{
-  const message = `Please don't leave any field empty`;
-
-  showMsg.style.display = 'none';
- alert(message)
-
-}
 }
 
 checkBtn.addEventListener("click", submitHandler);
